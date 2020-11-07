2387be6ef9b380ae47c6afe1a3f2519da35d022d
iamWebLuk <lukas.weber@edu.fh-joanneum.at>
2020-11-07 16:41:18 +0100

    first push

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function halo(){
+console.log("hallo alles klar? :-)");
+
+}
+
+function ifElse() {
+if (document.getElementById("p2").style.display="none" == true) {
+console.log("transparent") 
+  }  else {
+console.log("visible")
+    }
+}
+function demoShow() {
+    document.getElementById("p1").style.display="none";
+}
+
+function demoShow2() {
+    document.getElementById("p2").style.display="block";
+}
